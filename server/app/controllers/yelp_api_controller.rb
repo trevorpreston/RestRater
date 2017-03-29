e6e2c3627b0781ea267a3df297e4cf261787cd35
Trevor Preston <trevorpreston4@gmail.com>
2017-03-29 11:58:23 -0400

    add working query params to server

--- server/app/controllers/yelp_api_controller.rb
+++ server/app/controllers/yelp_api_controller.rb
@@ -2,6 +2,7 @@ class YelpApiController < ApplicationController
 	include HTTParty
 
   def index
+  	search_term = params[:queryterm]
   	yelp_key = "Bearer " + ENV['YELP_API_KEY']
     url = "https://api.yelp.com/v3/businesses/search"
     response =  HTTParty.get(
@@ -10,13 +11,14 @@ class YelpApiController < ApplicationController
     		Authorization: yelp_key
     	},
     	query: {
-    		term: "lucalis", 
+    		term: search_term, 
     		latitude: 40.762951,
     		longitude: -73.980833
-    	},
-    	debug_output: $stdout
+    	}
+    	# ,debug_output: $stdout
     )
     parsed_body = JSON.parse(response.body)
     render json: parsed_body
   end
+
 end
