f730e6eb39fe2ec49e46f0f530f32904d0bda633
Trevor Preston <trevorpreston4@gmail.com>
2017-04-09 20:11:57 -0400

    add scroll overflow to search results

--- frontend/src/SearchResultListItem.jsx
+++ frontend/src/SearchResultListItem.jsx
@@ -3,9 +3,9 @@ import React, { Component } from 'react'
 export default class SearchResultListItem extends Component {
 	render(){
 		return(
-			<div>
-				<p> {this.props.result.name} </p>
-				<p> {this.props.result.rating} </p>
+			<div className="search-result-item">
+				<p> restaurant: {this.props.result.name} </p>
+				<p> rating: {this.props.result.rating} </p>
 			</div>
 		)
 	}
