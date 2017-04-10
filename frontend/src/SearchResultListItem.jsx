f41c32f31ce8f684d3c4e321077af441263ac514
Trevor Preston <trevorpreston4@gmail.com>
2017-04-10 11:27:23 -0400

    add yelp styling to saved components

--- frontend/src/SearchResultListItem.jsx
+++ frontend/src/SearchResultListItem.jsx
@@ -3,8 +3,8 @@ import React, { Component } from 'react'
 export default class SearchResultListItem extends Component {
 	render(){
 		return(
-			<div className="search-result-item">
-				<p> restaurant: {this.props.result.name} </p>
+			<div className="result-item">
+				<p> name: {this.props.result.name} </p>
 				<p> rating: {this.props.result.rating} </p>
 			</div>
 		)
